194533f660d7a7baec08f993c126b83e9123dd9f
Jensone <hello@jensone.com>
2024-07-10 19:58:08 +0000

    📝 docs(login_controller.ts) : ajouter des commentaires expliquant la méthode show 📝 docs(logout_controller.ts) : ajouter des commentaires expliquant la méthode getOut

--- app/controllers/auth/login_controller.ts
+++ app/controllers/auth/login_controller.ts
@@ -2,6 +2,11 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class LoginController {
 
+  /**
+   * Retourne l'objet `view` et applique dessus
+   * la méthode `render` avec pour argument
+   * le chemin vers la vue `pages/auth/login`
+   */
   async show({ view }: HttpContext) {
     return view.render('pages/auth/login')
   }
