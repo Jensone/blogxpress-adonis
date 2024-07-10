57243cfa8f3a7aab0e052786018c0797aa016f52
Jensone <hello@jensone.com>
2024-07-10 15:21:50 +0000

    🚀 fonctionnalité(auth) : ajouter les contrôleurs de connexion, déconnexion et enregistrement 🚀 fonctionnalité(auth) : ajouter les vues pour la connexion, la déconnexion et l'enregistrement 🚀 fonctionnalité(auth) : définir les routes pour la connexion, la déconnexion et l'enregistrement

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,22 @@
 |
 */
 
+import LoginController from '#controllers/auth/login_controller'
+import LogoutController from '#controllers/auth/logout_controller'
+import RegisterController from '#controllers/auth/register_controller'
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home')
+
+router.group(() => {
+  // Registration
+  router.get('/register', [RegisterController,'show']).as('register.show')
+  router.post('/register', [RegisterController,'store']).as('register.store')
+
+  // Login
+  router.get('/login', [LoginController,'show']).as('login.show')
+  router.post('/login', [LoginController,'store']).as('login.store')
+
+  // Logout
+  router.post('/logout', [LogoutController,'getOut']).as('logout')
+}).as('auth')
